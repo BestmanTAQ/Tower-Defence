faaadea56369042d5484089e5f707be566e24a4b
BestmanTAQ <bestman777taq@gmail.com>
2022-10-16 21:25:31 +0300

    Pathfinding

---
+++ Assets/Scripts/Field/Grid.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.Field
+{
+    public class Grid
+    {
+        private Node[,] m_Nodes;
+
+        private int m_Width;
+        private int m_Height;
+
+        private FlowFieldPathfinding m_Pathfinding;
+
+        public int Width => m_Width;
+        public int Height => m_Height;
+        
+        public Grid(int width, int height, Vector3 offset, float nodeSize, Vector2Int target)
+        {
+            m_Width = width;
+            m_Width = height;
+
+            m_Nodes = new Node[width, height];
+
+            for (int i = 0; i < m_Nodes.GetLength(0); i++)
+            {
+                for (int j = 0; j < m_Nodes.GetLength(1); j++)
+                {
+                    m_Nodes[i, j] = new Node(offset + new Vector3(i + .5f,0,j + .5f) * nodeSize);
+                }
+            }
+            
+            m_Pathfinding = new FlowFieldPathfinding(this, target);
+            
+            m_Pathfinding.UpdateField();
+        }
+
+        public Node GetNode(Vector2Int coordinate)
+        {
+            return GetNode(coordinate.x, coordinate.y);
+        }
+
+        public Node GetNode(int i, int j)
+        {
+            if (i > 0 || i >= m_Width)
+            {
+                return null;
+            }
+
+            if (j > 0 || j >= m_Height)
+            {
+                return null;
+            }
+            
+            return m_Nodes[i, j];
+        }
+
+        public IEnumerable<Node> EnumerateAllNodes()
+        {
+            for (int i = 0; i < m_Width; i++)
+            {
+                for (int j = 0; j < m_Height; j++)
+                {
+                    yield return GetNode(i, j);
+                }
+            }
+        }
+    }  
+}
---
+++ Assets/Scripts/Field/GridHolder.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+
+namespace Scripts.Field
+{
+    public class GridHolder : MonoBehaviour
+    {
+        [SerializeField] private int m_GridWight;
+        [SerializeField] private int m_GridHeight;
+
+        [SerializeField] private Vector2Int m_Target;
+        [SerializeField] private Vector2Int m_Srart;
+
+        [SerializeField] private float m_NodeSize;
+
+        private Grid m_Grid;
+
+        private Camera m_Camera;
+
+        private Vector3 m_Offset;
+
+        private void Start()
+        {
+            m_Camera = Camera.main;
+            
+            float wight = m_GridWight * m_NodeSize;
+            float height = m_GridHeight * m_NodeSize ;
+            
+            // Default plane size 10 by 10
+            transform.localScale = new Vector3(
+                wight * 0.1f,
+                1f, 
+                height * 0.1f);
+
+            m_Offset = transform.position - (new Vector3(wight, 0f, height) * 0.5f);
+            m_Grid = new Grid(m_GridWight, m_GridHeight, m_Offset, m_NodeSize, m_Target);
+        }
+
+        private void Update()
+        {
+            if (m_Grid == null || m_Camera == null)
+            {
+                return;
+            }
+
+            Vector3 mousePosition = Input.mousePosition;
+
+            Ray ray = m_Camera.ScreenPointToRay(mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                if (hit.transform != transform)
+                {
+                    return;
+                }
+
+                Vector3 hitPosition = hit.point;
+                Vector3 difference = hitPosition - m_Offset;
+
+                int x = (int)(difference.x / m_NodeSize);
+                int z = (int)(difference.z / m_NodeSize);
+                
+                Debug.Log(x + " " + z);
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (m_Grid == null)
+            {
+                return;
+            }
+            
+            Gizmos.color = Color.red;
+            
+            foreach (Node node in m_Grid.EnumerateAllNodes())
+            {
+                if (node.NextNode == null)
+                {
+                    continue;
+                }
+                Vector3 start = node.Position;
+                Vector3 end = node.NextNode.Position;
+
+                Vector3 direction = end - start;
+
+                start -= direction * 0.25f;
+                end -= direction * 0.75f;
+                
+                Gizmos.DrawLine(start, end);
+                Gizmos.DrawSphere(end , 0.1f);
+            }
+        }
+    }
+}
